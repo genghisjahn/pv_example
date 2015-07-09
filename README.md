1e1c649b7c93416ed50a9b59d30c81ccd8a235d2
Jon Wear <jon@dramafever.com>
2015-07-08 19:53:46 -0600

    ready to talk about now

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pv_example
 Examples of different behaviors between pointers &amp; values in go.
+
+
+http://golang.org/doc/faq#methods_on_values_or_pointers
--- main.go
+++ main.go
@@ -7,26 +7,19 @@ type person struct {
 	age  int
 }
 
-type animal struct {
-	name string
-	age  int
-}
-
 type greeter interface {
 	Greet() string
 }
-
-type server interface {
-	TakeOrder([]string)
+type sleeper interface {
+	Sleep() string
 }
 
 func main() {
 	p1 := person{"Jon", 41}
-	a1 := animal{"Tumnus", 8}
-	personstuffV(p1)
-	personstuffP(&p1)
 
-	doGreet(&a1)
+	personV(p1)
+	personP(&p1)
+
 	doGreet(&p1)
 	/*
 		doGreet(p1)
@@ -34,38 +27,61 @@ func main() {
 		The above won't compile.  You'll get:
 		cannot use p1 (type person) as type greeter in argument to doGreet: person does not implement greeter (Greet method has pointer receiver)
 
+		If a method is defined on a pointer receiver, it can only be
+		used in an interface if a pointer is passed into the interface.
 	*/
 
-	doServe(&p1)
-	//doServe(p1) doesn't work, explain why
-}
+	goSleep(p1)
+	goSleep(&p1)
 
-func (a *animal) Greet() string {
-	return fmt.Sprintf("ArfArfArfBowWow!!")
+	/*
+		Sleep() is defined on a value receiver, so you can pass in
+		values or pointers to functions
+		that require the Sleeper interface
+	*/
+
 }
 
 func (p *person) Greet() string {
 	return fmt.Sprintf("I'm %v and I'm %v years old.", p.name, p.age)
-
 }
 
-func (p *person) TakeOrder(items []string) {
-	fmt.Println("I already forgot the order.")
+func (p person) Sleep() string {
+	return "zzzzz"
 }
 
-func personstuffV(p person) {
+func personV(p person) {
+	//Greet() is defined on a pointer,
+	//but we can still call it
+	//even though p is a value param.
+	fmt.Println("--Person Value--")
+	fmt.Printf("Name %v\nAge:%v\n", p.name, p.age)
 	fmt.Println(p.Greet())
+	fmt.Printf("Type: %T\n", p)
+	fmt.Printf("\n\n")
 }
 
-func personstuffP(p *person) {
-	fmt.Println(p.Greet())
+func personP(p *person) {
+	//Sleep() is defined on a value receiver,
+	//but we can still call it from a pointer
+	fmt.Println("--Person Pointer--")
+	fmt.Printf("Name %v\nAge:%v\n", p.name, p.age)
+	fmt.Println(p.Sleep())
+	fmt.Printf("Type: %T\n", p)
+	fmt.Printf("\n\n")
+
 }
 
 func doGreet(g greeter) {
-	fmt.Println(g.Greet())
+	fmt.Println("--doGreet--")
+	fmt.Println("Greeting:", g.Greet())
+	fmt.Printf("Type: %T\n", g)
+	fmt.Printf("\n\n")
 }
 
-func doServe(s server) {
-	items := []string{"Fries", "Icecream"}
-	s.TakeOrder(items)
+func goSleep(s sleeper) {
+	fmt.Println("--gosleep--")
+	fmt.Println("Sleeping:", s.Sleep())
+	fmt.Printf("Type: %T\n", s)
+	fmt.Printf("\n\n")
 }
